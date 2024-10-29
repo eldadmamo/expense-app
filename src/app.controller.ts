a0c0174ad2b1cc2e04e4db93300460b99d5d1196
eldadstar <eldadfikre456@gmail.com>
2024-10-30 00:04:38 +0300

    updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,30 @@
-import {Controller, Get} from "@nestjs/common"
+import {Controller, Delete, Get, Post, Put} from "@nestjs/common"
 
-@Controller('hi')
+@Controller('report/income')
 export class AppController {
-  @Get('')
+  @Get()
   getAllIncomeReports(){
     return []
   }
 
-  @Get('hello')
-  getAllIncomeReport(){
-    return {}
+  @Get(':id')
+  getIncomeReportById(){
+    return {"hello":"hi"}
+  }
+
+  @Post() 
+  postIncome(){
+    return ["POST"]
+  }
+
+  @Put(':id')
+  putIncome(){
+    return ["Put"]
+  }
+
+  @Delete(":id")
+    deleteIncome(){
+      return ["Delete"]
   }
 }
 
